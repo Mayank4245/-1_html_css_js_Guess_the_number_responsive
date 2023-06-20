e174283f7479cd591a622f4f23332514686dd7fd
Mayank <mayank.agrawalbca@gmai.com>
2023-06-20 15:33:43 +0530

    Update some js code for mobile

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
+
 let number = Math.floor(Math.random() * 101);
-var chance = 0 ;
-function guessTheNumber(){
-    let input = document.getElementById("input2").value;
-    if(input == number){
-        document.getElementById("output_comment").innerHTML = `Congratulations....! You Enter Right Number`
-        chance = chance+1;
-        document.getElementById("chance").innerHTML = `No. of Guesses : ${chance}`
-    }else if(input < number){
-        document.getElementById("output_comment").innerHTML = `Ohh ho...! You enter ${input} which is wrong! Try again with greater number`
-        chance = chance+1;
-        document.getElementById("chance").innerHTML = `No. of Guesses : ${chance}`
-    }
-    else{
-        document.getElementById("output_comment").innerHTML = `Ohh ho...! You enter ${input} which is wrong! Try again with smaller number`
-        chance = chance+1;
-        document.getElementById("chance").innerHTML = `No. of Guesses : ${chance}`
-    }
+let chance = 0;
+
+function guessTheNumber() {
+  let input = parseInt(document.getElementById("input2").value);
+
+  if (input === number) {
+    document.getElementById("output_comment").textContent = "Congratulations! You entered the right number.";
+  } else if (input < number) {
+    document.getElementById("output_comment").textContent = `Oh no! You entered ${input}, which is too low. Try again with a greater number.`;
+  } else {
+    document.getElementById("output_comment").textContent = `Oh no! You entered ${input}, which is too high. Try again with a smaller number.`;
+  }
+
+  chance++;
+  document.getElementById("chance").textContent = `No. of Guesses: ${chance}`;
 }
